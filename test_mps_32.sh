633ef84710a712bb05c07d5cb24affa16deace7c
MuyeX <muye.xiao19@imperial.ac.uk>
2025-12-03 14:45:01 +0800

    change to LSTM

--- test_mps_32.sh
+++ test_mps_32.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 python main_MPS.py \
   --mode test \
   --method LSTM \
-  --model_path ./logs/train/RNN_20251203_0630/model.pt      \
+  --model_path ./logs/train/LSTM_20251203_0640/model.pt      \
   --data_path ./datasets/simglucose/Simulation_OpenAPS_testing_all_faults/ \
   --log_dir logs/
